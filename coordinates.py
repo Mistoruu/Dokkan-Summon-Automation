98c177fd94c387141805a02c3dcf9a1b111a6d20
Mistoruu <150136429+Mistoruu@users.noreply.github.com>
2024-09-08 23:43:20 +0200

    Add files via upload

---
+++ coordinates.py
@@ -0,0 +1,4 @@
+import pyautogui
+while True:
+    print(pyautogui.position())  # Affiche la position actuelle de la souris
+    pyautogui.sleep(1)  # Attend 1 seconde avant de mettre à jour la position
